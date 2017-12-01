5389ee790462bb91b9f8b29ebed3e296442c88c2
yangfuwei1988 <33475843+yangfuwei1988@users.noreply.github.com>
2017-12-01 11:19:00 +0800

    增加活动预告
    增加活动预告

---
+++ files/5_2_搜索结果列表/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(Q,_()),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="5_2_搜索结果列表.html",d="generationDate",e=new Date(1512098320570.32),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="9f1c65c4e0e0466fb433aea8d2152173",n="type",o="Axure:Page",p="name",q="5.2 搜索结果列表",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="a",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
